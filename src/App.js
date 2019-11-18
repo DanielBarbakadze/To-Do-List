b07a41bbce026c6b523cb85153f16910c65804c1
DanielBarbakadze <danielbarbakadze@gmail.com>
2019-11-18 17:12:19 +0400

    add routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Login, Registration, Main } from './pages'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/Login">Login</Link>
+            </li>
+            <li>
+              <Link to="/Registration">Registration</Link>
+            </li>
+            <li>
+              <Link to="/Main">Main</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+
+          <Route path="/Login">
+            <Login />
+          </Route>
+
+          <Route path="/Registration">
+            <Registration />
+          </Route>
+
+          <Route path="/Main">
+            <Main />
+          </Route>
+
+          {/* </Route> */}
+          <Route path="/">
+            <Login />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
