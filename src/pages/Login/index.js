e6d8651c4ea1953fb53ef385135b487aa1280b76
AlexanderM11 <sandromutafiani@gmail.com>
2019-11-18 18:23:37 +0400

    login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
 export default function Login() {
-    return <h2>Login</h2>;
-  }
+  
+  return (
+  <div>
+    <header className="modeContainer">
+        <button className="modeButton">Light Mode</button>
+        <button className="modeButton">Dark Mode</button>
+    </header>
+    <section className="loginBox">
+      <label>Log In</label>
+    </section>
+  </div>
+  )
+}
