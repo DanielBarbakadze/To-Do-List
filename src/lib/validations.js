b36c49daeaf6d5c66dfa6b49239969f8cd0b4660
primulax <primulax@live.com>
2019-11-25 17:02:04 +0400

    registration page, validations, date input small styling

--- src/lib/validations.js
+++ src/lib/validations.js
@@ -1,6 +1,38 @@
-export function require(value){
+function require(value){
     if(value.length > 0 ) {
         return true;
     }
     else return false;
+}
+
+function usernameValidation(value){
+    /* იუზერნეიმი 4-დან 15 სიმბოლომდე, არ დაიწყებს _ ან . */
+    /* იუზერნეიმში არ შეიძლება __ ან _. ან ._ ან .. */
+    /* დაშვებული სიმბოლოები a-z-მდე და A-Z-მდე და 0-9-მდე */
+    /* იუზერნეიმის ბოლოში არ შეიძლება _ ან .*/
+    if (!/^(?=.{4,15}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/.test(value)) {
+        return true;
+    }
+    return false;
+}
+
+function fullnameValidation (value) {
+    if (!/^(?=.{2,40}$)[A-Za-z]+/.test(value)) {
+        return true;
+    }
+    return false;
+}
+
+function emailValidation (value) {
+    if (!/\S+@\S+\.\S+/.test(value)){
+        return true;
+    }
+    return false;
+}
+
+export default {
+    require,
+    usernameValidation,
+    fullnameValidation,
+    emailValidation
 }
--- src/pages/Registration/index.js
+++ src/pages/Registration/index.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Registration() {
-    return <h2>Registration</h2>;
+    return (
+
+      <div className="login-page">
+
+          <div className="form">
+
+            <div className="form-text">
+            <label>Registration</label>
+            </div>
+            
+            <form className="login-form">
+
+              <input type="text" name="username" placeholder="Enter Username"/>
+              <input type="text" name="firstname" placeholder="Enter Name"/>
+              <input type="text" name="surname" placeholder="Enter Surname"/>
+              <input className="date" type="date" name="date" placeholder="Your Birth Date"/>
+              <input type="email" name="email" placeholder="Enter Email"/>
+              <input type="password" name="password" placeholder="Enter Password"/>
+              <input type="password"  name="confirm" placeholder="Confirm Password"/>
+
+              <button>Register</button>
+              <p className="message">Registered? <Link to="/">
+                Login Jigaro!</Link></p>
+
+            </form>
+          </div>
+        </div>
+
+      )
   }
