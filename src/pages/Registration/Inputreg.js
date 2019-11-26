fc01a3ed2290850e51a30bce453e33c30518908b
ivatopuria <topuria.vano@gmail.com>
2019-11-26 08:29:17 -0500

    placeholders fix

--- src/pages/Registration/Inputreg.js
+++ src/pages/Registration/Inputreg.js
@@ -9,13 +9,17 @@ function InputReg(props){
     function handleChange(e){
        props.onChange(e);
     }
+    function changePlaceHolder(name){
+      if(name==="confirm password") return("Confirm password");
+      else return("Enter " + name);
+    }
     return(
        <div>
 
         <input 
         name = {props.name} 
         type = {props.type}
-        placeholder ={props.name}
+        placeholder ={changePlaceHolder(props.name)}
         className = {props.name}
         onChange ={((e) => handleChange(e))}
          />
