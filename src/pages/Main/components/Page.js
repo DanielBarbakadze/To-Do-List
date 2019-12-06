f9de9415a342d941c4c664f8b1cab0726fdf9afc
DanielBarbakadze <danielbarbakadze@gmail.com>
2019-12-06 14:04:36 +0400

    ADD 'already exists' checker during creating new project

--- src/pages/Main/components/Page.js
+++ src/pages/Main/components/Page.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import Main from './Main';
+import '../../../App.css';
 class Page extends React.Component {
   state = {
-    projects: []
+    projects: [],
+    existedErrorMessage: ''
   }
   handleChange(e) {
     this.setState({ inputState: e.target.value });
   }
   handleClick() {
     let array = this.state.projects;
+    
+    // check if the same input exists
+    let tempChecker = false;
+    array.map((el)=>{if(el==this.state.inputState)tempChecker=true})
+
     if (this.state.inputState != undefined && this.state.inputState != '') {
-      array.push(this.state.inputState);
-      this.setState({
-        projects: array
-      });
+      
+      if(tempChecker==false){
+        array.push(this.state.inputState);
+        this.setState({
+          projects: array,
+          existedErrorMessage: ''
+        });
+      }
+      else{
+        this.setState({
+          existedErrorMessage: this.state.inputState + " Already Exists"
+        })
+      }
     }
   }
 
@@ -28,6 +44,10 @@ class Page extends React.Component {
         <button onClick={() => this.handleClick()} >
           Create project
         </button>
+        <span>
+          <div className="error-text2">{this.state.existedErrorMessage}</div>
+        </span>
+        
         {
           this.state.projects.map((el) =>
             <li onClick={() => this.handleSelect(el,this.state.projects)}>
