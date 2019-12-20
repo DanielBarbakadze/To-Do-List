8faf8104c236aac17b7f0fcd5502314cf9982c36
Iva topuria <amcybersraui@Macintosh.local>
2019-12-21 00:06:43 +0400

    added kvazimodo Drag and Drop

--- src/pages/Main/components/Main.js
+++ src/pages/Main/components/Main.js
@@ -3,13 +3,15 @@ import List from './List';
 class Main extends React.Component {
   state = {
     list: this.props.list,
-    inputState: ""
+    inputState: "",
+    firstValue:1,
+     secondValue:1
   }
   handleClick() {
     let lastItem;
     if (this.state.inputState != "") {
       let tempStateList = this.state.list;
-      if (tempStateList.length == 0) lastItem = { id: 1, value: this.state.inputState };
+      if (tempStateList.length == 0) lastItem = { id: 0, value: this.state.inputState };
       else {
         lastItem = { id: this.state.list[this.state.list.length - 1].id + 1, value: this.state.inputState };
       }
@@ -35,6 +37,42 @@ class Main extends React.Component {
     })
     this.props.handleList(tempList);
   }
+
+  dragStart(event){
+    let id = event.target.id;
+    if(id!=""){
+      this.setState({firstValue:id});
+    }
+   }
+
+   drop(event){
+    let list = this.state.list;
+    let firstVal = this.state.firstValue;
+    let secondVal = this.state.secondValue;
+    firstVal*=1; secondVal*=1;
+    let toswap = 0;
+    let curr = 0;
+    list.map((item) => {if(item.id==secondVal) toswap = item
+    }
+    );
+    list.map((item) => {if(item.id==firstVal) curr = item
+    });
+    let newList = list.map((item)=>{
+      if(item.id === firstVal) return toswap;
+      else if(item.id==secondVal) return curr;
+      else return item;
+    });
+    this.setState({list:newList});
+   }
+
+   dragEnter(event){
+    event.preventDefault();
+     let id = event.target.id;
+    if(id!=""){
+      this.setState({secondValue:id});
+    }
+    }
+    
   render() {
     return (
       <div> 
@@ -58,6 +96,13 @@ class Main extends React.Component {
          </div>
        </div>    
           {this.state.list.map((item) =>
+          <div>
+            <li 
+            id = {item.id} 
+            draggable = 'true'
+             onDragStart = {(event) =>this.dragStart(event)} 
+            onDragEnd = {(event) => this.drop(event)}
+             onDragEnter={(event) => this.dragEnter(event)}> 
             <List
               id={item.id}
               value={item.value}
@@ -66,6 +111,9 @@ class Main extends React.Component {
               handleDelete={(e) => this.handleDelete(e)}
               handleLine={(e) => this.handleLine(e)}
             />
+            </li>
+            <br/>
+             </div>
           )}
         
        </div>
